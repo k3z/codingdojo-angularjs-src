1211293c2c895e7c4892f53e0a6c59510f0697d6
Olivier Kautz <olivier@k3z.fr>
2013-07-07 21:00:08 +0200

    Added app module and first controller

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -3,10 +3,39 @@
 */
 
 
-// var app = angular.module('app', ['ui.bootstrap']);
+var app = angular.module('app', []);
 
-// app.run(function ($rootScope) {
+app.run(function ($rootScope) {
 
-//     $rootScope.project_name = 'PodcastHub';
+    $rootScope.project_name = 'PodcastHub';
 
-// });
+});
+
+
+app.controller('PodcastsCtrl', function ($scope, $rootScope) {
+
+    $scope.project_name = $rootScope.project_name;
+
+    $scope.podcasts = {
+        "items": [
+            {
+                "label": "Geek Inc",
+                "slug": "geek-inc",
+                "audio": true,
+                "video": true,
+                "live": true,
+                "episodes": 150,
+                "stars": 5
+            },
+            {
+                "label": "Positron",
+                "slug": "positron",
+                "audio": true,
+                "video": false,
+                "live": false,
+                "episodes": 1,
+                "stars": 5
+            }
+        ]
+    }
+});
